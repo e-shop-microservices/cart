eca763b6d8307fd5c3ca24960e50b70780a2b6bb
Oles Pankiv <ojlesua@gmail.com>
2019-05-29 14:23:24 +0300

    Move configuratino to configserver

--- src/main/java/ojles/coursework/cart/configuration/CatalogueConfiguration.java
+++ src/main/java/ojles/coursework/cart/configuration/CatalogueConfiguration.java
@@ -1,15 +1,13 @@
 package ojles.coursework.cart.configuration;
 
 import ojles.coursework.cart.service.CatalogueService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CatalogueConfiguration {
-    @Value("${catalogue.url}")
-    private String catalogueUrl;
+    private String catalogueUrl = "http://localhost:8081";
 
     @Bean
     public CatalogueService catalogueService() {
